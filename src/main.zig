3475b81e6282f78393cb25ea74358cb7d0d73c86
4zv4l <4zv4l@protonmail.com>
2024-08-06 09:25:15 +0800

    up: resolveIp -> parseIp to allow cross-compilation

--- src/main.zig
+++ src/main.zig
@@ -128,7 +128,7 @@ pub fn main() !void {
     // parse address
     const ip = args.options.bind;
     const port = try std.fmt.parseUnsigned(u16, args.positionals[0], 10);
-    const addr = try net.Address.resolveIp(ip, port);
+    const addr = try net.Address.parseIp(ip, port);
 
     // remove trailing / from directory path
     const dir = std.mem.trimRight(u8, args.options.directory, "/");
